0b2a6a2735a3ada00945e195e415c67f886d8d60
Dmitriy Brovchenko <dbrov@mail.ru>
2020-04-23 19:05:01 +0300

    третья попытка четвертой работы

---
+++ scripts/script.js
@@ -0,0 +1,33 @@
+const editBtn = document.querySelector('.edit-button');
+const form = document.querySelector('.popup');
+const closeBtn = document.querySelector('.close-button');
+
+const nameInput = document.querySelector('.popup__item_el_name');
+const jobInput = document.querySelector('.popup__item_el_prof');
+const profTitle = document.querySelector('.profile__title');
+const profSubt = document.querySelector('.profile__subtitle');
+
+
+function edit() {
+    form.classList.add ('popup_opened');
+    nameInput.value = profTitle.textContent;
+    jobInput.value = profSubt.textContent;
+}
+
+function close() {
+    form.classList.remove('popup_opened');
+}
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();      
+
+    profTitle.textContent =  nameInput.value;
+    profSubt.textContent =  jobInput.value;
+
+    close();
+}
+
+
+editBtn.addEventListener("click", edit); 
+closeBtn.addEventListener("click", close); 
+form.addEventListener('submit', formSubmitHandler);
